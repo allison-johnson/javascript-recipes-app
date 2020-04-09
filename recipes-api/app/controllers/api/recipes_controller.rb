f4ec90692852adf13c226ef4402ef646ec94711e
allison_johnson <allison.cuttler@gmail.com>
2020-04-09 08:09:59 -0700

    basic form for adding recipe

--- recipes-api/app/controllers/api/recipes_controller.rb
+++ recipes-api/app/controllers/api/recipes_controller.rb
@@ -11,7 +11,23 @@ class Api::RecipesController < ApplicationController
   def show
     recipe = Recipe.find_by(id: params[:id])
     render json: recipe 
-  end
+  end #show
+
+  #POST/recipes
+  def create
+    recipe = Recipe.new(recipe_params);
+    if recipe.save
+      render json: recipe, status: :created
+    else
+      render json: recipe.errors, status: :unprocessable_entity
+    end
+  end #create
+
+  private
+  #Only allow a trusted parameter "white list" through
+  def recipe_params
+    params.require(:recipe).permit(:name, :url, :img_url)
+  end #recipe_params
 
 
 end #class 
